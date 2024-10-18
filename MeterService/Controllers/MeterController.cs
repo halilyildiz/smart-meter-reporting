ee605d6a0e5d8716b9e93261fc9aa750327aff98
Halil Yıldız <halilyildiz.ds@gmail.com>
2024-10-18 22:58:16 +0300

    Sayaç son ölçüm verisinin getirilmesini sağlayan ve sayaç ölçüm verisinin kaydedilmesini sağlayan http metotlarının oluşturulması

--- MeterService/Controllers/MeterController.cs
+++ MeterService/Controllers/MeterController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MeterService.Data;
+using MeterService.Entities;
+using MeterService.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MeterService.Controllers
 {
@@ -6,10 +9,51 @@ namespace MeterService.Controllers
   [Route("api/[controller]")]
   public class MeterController : ControllerBase
   {
-    [HttpGet]
-    public IActionResult GetMeters()
+    private readonly MeterDbContext _context;
+
+    public MeterController(MeterDbContext context)
     {
-      return Ok("Meter data");
+      _context = context;
     }
+
+    [HttpGet("{serialNumber}")]
+    public ActionResult<Meter> GetLastMeasurement(string serialNumber)
+    {
+      var meter = _context.Meters
+          .Where(m => m.MeterSerialNumber == serialNumber)
+          .OrderByDescending(m => m.MeasurementTime)
+          .FirstOrDefault();
+
+      if (meter == null)
+      {
+        return NotFound();
+      }
+
+      return Ok(meter);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Meter>> SaveMeasurement([FromBody] MeterMeasurementRequest meterRequest)
+    {
+      if (meterRequest == null)
+      {
+        return BadRequest();
+      }
+
+      var meter = new Meter() 
+      { 
+        MeterSerialNumber = meterRequest.MeterSerialNumber, 
+        LastIndex = meterRequest.LastIndex, 
+        Current = meterRequest.Current, 
+        Voltage = meterRequest.Voltage, 
+        MeasurementTime = meterRequest.MeasurementTime 
+      };
+
+      _context.Meters.Add(meter);
+      await _context.SaveChangesAsync();
+
+      return CreatedAtAction(nameof(GetLastMeasurement), new { serialNumber = meter.MeterSerialNumber }, meter);
+    }
+
   }
 }
---
+++ MeterService/Models/MeterMeasurementRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace MeterService.Models
+{
+  public class MeterMeasurementRequest
+  {
+    public required string MeterSerialNumber { get; set; }
+    public DateTime MeasurementTime { get; set; }
+    public decimal LastIndex { get; set; }
+    public decimal Voltage { get; set; }
+    public decimal Current { get; set; }
+  }
+}
